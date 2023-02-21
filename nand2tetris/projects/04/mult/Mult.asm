b8a303348385dfd3a9709803419d4aeb2cbd0563
Sotatek-DuyNguyen <duy.nguyen4@sotatek.com>
2023-02-20 18:15:59 -0600

    Machine Language

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,44 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@R2
+M = 0
+
+@R1
+D = M
+
+@temp
+M = D
+
+@product
+M = 0
+
+(LOOP)
+    @temp
+    D = M
+
+    @END
+    D; JEQ
+
+    @R0
+    D = M
+
+    @product
+    M = M+D
+
+    @temp
+    M = M-1
+
+    @LOOP
+    0; JMP
+
+(END)
+    @product
+    D =M
+    @R2
+    M = D
+
+    @END
+    0; JMP
+
